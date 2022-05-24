e590962316046d804392f48fe8c488dcaeb76770
Alejo9806 <mello0616@gmail.com>
2022-05-24 12:44:01 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import {BrowserRouter,Routes,Route} from 'react-router-dom';
+import AuthLayout from './layout/AuthLayout';
+import ConfirmarCuenta from './pages/ConfirmarCuenta';
+import Login from './pages/Login';
+import OlvidePassword from './pages/OlvidePassword';
+import Registrar from './pages/Registrar';
+
+function App() {
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<AuthLayout/>}>
+          <Route index element={<Login/>} />
+          <Route path='confirmar/:id' element={<ConfirmarCuenta/>} />
+          <Route path='registrar' element={<Registrar/>} />
+          <Route path='olvide-password' element={<OlvidePassword/>} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  )
+
+}
+
+export default App
